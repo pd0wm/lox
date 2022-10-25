c56b4a2e58f178eb90fa31232f84bfe3fa0d39d5
Willem Melching <willem.melching@gmail.com>
2022-10-25 11:57:56 +0200

    callable using enum

--- src/ast.rs
+++ src/ast.rs
@@ -1,5 +1,3 @@
-use std::fmt;
-
 use crate::token::{Literal, Token};
 
 pub enum Expr {
@@ -11,6 +9,11 @@ pub enum Expr {
         left: Box<Expr>,
         operator: Token,
         right: Box<Expr>,
+    },
+    Call {
+        callee: Box<Expr>,
+        paren: Token,
+        arguments: Vec<Box<Expr>>,
     },
     Grouping {
         expression: Box<Expr>,
@@ -55,62 +58,4 @@ pub enum Stmt {
         condition: Box<Expr>,
         body: Box<Stmt>,
     },
-}
-
-fn parenthesize(f: &mut fmt::Formatter, name: &str, exprs: &[&Box<Expr>]) -> fmt::Result {
-    write!(f, "({}", name)?;
-    for expr in exprs {
-        write!(f, " {}", expr)?;
-    }
-    write!(f, ")")
-}
-
-impl fmt::Display for Expr {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Expr::Assign { name, value } => write!(f, "var {} = {}", name, value),
-            Expr::Binary {
-                left,
-                operator,
-                right,
-            } => parenthesize(f, &operator.lexeme, &[left, right]),
-            Expr::Grouping { expression } => parenthesize(f, "group", &[expression]),
-            Expr::Literal { value } => write!(f, "{}", value),
-            Expr::Logical {
-                left,
-                operator,
-                right,
-            } => write!(f, "{} {} {}", left, &operator.lexeme, right),
-            Expr::Unary { operator, right } => parenthesize(f, &operator.lexeme, &[right]),
-            Expr::Variable { name } => write!(f, "var {}", name),
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::token_type::TokenType;
-
-    use super::*;
-
-    #[test]
-    fn test_fmt() {
-        // Example from 5.4
-        let expression = Expr::Binary {
-            left: Box::new(Expr::Unary {
-                operator: Token::new(TokenType::Minus, "-", None, 1),
-                right: Box::new(Expr::Literal {
-                    value: Literal::Number(123.0),
-                }),
-            }),
-            operator: Token::new(TokenType::Star, "*", None, 1),
-            right: Box::new(Expr::Grouping {
-                expression: Box::new(Expr::Literal {
-                    value: Literal::Number(45.67),
-                }),
-            }),
-        };
-
-        assert_eq!("(* (- 123) (group 45.67))", expression.to_string());
-    }
 }
