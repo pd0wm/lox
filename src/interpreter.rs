a4f7de714fbfaceff0bead83bc9e4bafa7856e25
Willem Melching <willem.melching@gmail.com>
2022-10-25 13:17:13 +0200

    move native functions into file

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,9 +1,8 @@
-use std::time::{SystemTime, UNIX_EPOCH};
-
 use crate::ast::{Expr, Stmt};
 use crate::environment::Environment;
 use crate::lox_error::{LoxError, RuntimeError};
-use crate::token::{Callable, Literal, NativeFunction, Token};
+use crate::native_functions::setup_native_functions;
+use crate::token::Literal;
 use crate::token_type::TokenType;
 
 fn is_truthy(val: &Literal) -> bool {
@@ -26,28 +25,6 @@ fn is_equal(left: &Literal, right: &Literal) -> bool {
 
 pub struct Interpreter {
     environment: Environment,
-}
-
-// TODO: move into native_functions.rs
-fn clock_fn(_interpreter: &Interpreter, _arguments: &Vec<Literal>) -> Result<Literal, LoxError> {
-    let now = SystemTime::now();
-    let secs = now.duration_since(UNIX_EPOCH).unwrap().as_secs_f64();
-    Ok(Literal::Number(secs))
-}
-
-fn setup_native_functions(environment: &mut Environment) {
-    environment.define(
-        &Token {
-            type_: TokenType::Fun,
-            lexeme: "clock".to_string(),
-            literal: None,
-            line: 0,
-        },
-        &Literal::Callable(Callable::NativeFunction(NativeFunction {
-            arity: 0,
-            closure: clock_fn,
-        })),
-    );
 }
 
 impl Interpreter {
